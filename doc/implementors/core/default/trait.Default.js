011f7a3ce78aa9b50ccad4a61434d770eee69fda
Steven Fackler <sfackler@gmail.com>
2015-02-18 05:06:07 +0000

    rebuild pages at 35a8d648b3d42e319ee58a1e3cdf0337b3e0108e

--- doc/implementors/core/default/trait.Default.js
+++ doc/implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['r2d2'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='r2d2/struct.Config.html' title='r2d2::Config'>Config</a>",];
+implementors['r2d2'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='r2d2/config/struct.Config.html' title='r2d2::config::Config'>Config</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
