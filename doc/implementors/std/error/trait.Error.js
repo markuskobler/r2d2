1ab1dac45bbfe309e22590eb6bbd975d8e6177d6
Steven Fackler <sfackler@gmail.com>
2015-04-02 18:50:46 +0000

    rebuild pages at e620f330721209a84caca5c55fcecce47e51d1e2

---
+++ doc/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['r2d2'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='r2d2/struct.InitializationError.html' title='r2d2::InitializationError'>InitializationError</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='r2d2/struct.GetTimeout.html' title='r2d2::GetTimeout'>GetTimeout</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
